9fab465d06f12994f92faff78edc74f617d00e7f
David Dollar <ddollar@gmail.com>
2012-06-17 16:48:06 -0400

    lock around multiplexer output

--- lib/distributor/multiplexer.rb
+++ lib/distributor/multiplexer.rb
@@ -1,6 +1,7 @@
 require "distributor"
 require "distributor/okjson"
 require "distributor/packet"
+require "thread"
 
 class Distributor::Multiplexer
 
@@ -8,6 +9,7 @@ class Distributor::Multiplexer
     @output  = output
     @readers = {}
     @writers = {}
+    @write_lock = Mutex.new
 
     @output.sync = true
   end
@@ -36,7 +38,9 @@ class Distributor::Multiplexer
   end
 
   def output(ch, data)
-    Distributor::Packet.write(@output, ch, data)
+    @write_lock.synchronize do
+      Distributor::Packet.write(@output, ch, data)
+    end
   rescue Errno::EPIPE
   end
 
