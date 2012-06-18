412abb7ca12ac58d8762cf7147a995a8b5dfd034
David Dollar <ddollar@gmail.com>
2012-06-18 10:50:36 -0400

    better example with comments

--- example/local.rb
+++ example/local.rb
@@ -10,29 +10,36 @@ def set_buffer(enable)
   enable ? `stty icanon echo` : `stty -icanon -echo`
 end
 
+### SERVER
+
 if ARGV.first == "server"
 
-  begin
-    server = Distributor::Server.new($stdin.dup, $stdout.dup)
-    $stdout = $stderr
+  # create a server that uses stdin/stdout for communication
+  server = Distributor::Server.new($stdin.dup, $stdout.dup)
+  $stdout.reopen $stderr
 
-    server.on_command do |command, data|
-      server.command command, data.merge("ack" => Time.now.to_i)
-    end
-
-    server.start
-  rescue Interrupt
+  # echo commands back with an ack parameter
+  server.on_command do |command, data|
+    server.command command, data.merge("ack" => Time.now.to_i)
   end
+
+  server.start
+
+### CLIENT
 
 else
 
   begin
+
+    # create a client that talks to a server created over a subprocess
     client = Distributor::Client.new(IO.popen("ruby subshell.rb server", "w+"))
 
+    # output the results of ls -la to test.log, with no input channel
     client.run("ls -la") do |ch|
       client.hookup ch, nil, File.open("test.log", "a+")
     end
 
+    # run a subshell and hook it up to stdin/stdout
     client.run("bash 2>&1") do |ch|
       client.hookup ch, $stdin.dup, $stdout.dup
       client.on_close(ch) do
@@ -40,28 +47,40 @@ else
       end
     end
 
+    # echo commands to stdout
     client.on_command do |command, data|
-      p [:received, command, data]
+      $stdout.puts "received: #{command} #{data.inspect}"
     end
 
-    client.command "foo", "bar" => "baz"
+    # send a command to the server
+    client.command "test", "foo" => "bar"
 
+    # create a proxy on localhost:8000 to be the local endpoint for a tunnel
     tcp = TCPServer.new(8000)
 
     Thread.new do
       loop do
         Thread.start(tcp.accept) do |tcp_client|
-          client.tunnel(nil) do |ch|
+
+          # connect to localhost:5000 on the server and tunnel it back out
+          client.tunnel(5000) do |ch|
             client.hookup ch, tcp_client
           end
+
         end
       end
     end
 
+    # turn off terminal specials
     set_buffer false
+
     client.start
+
   ensure
+
+    # restore terminal
     set_buffer true
+
   end
 
 end
